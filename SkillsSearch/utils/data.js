a6db476a17bfefcf6cab45dac9cb88b1e7a3cbb3
Nishchaya Narula <n.nishchaya2000@gmail.com>
2025-12-25 23:36:59 +0530

    feat: Virtualization_Added

--- SkillsSearch/utils/data.js
+++
@@ -1,9 +0,0 @@
-export const data = {
-    id: 1,
-    skills: [
-        'TypeScript', 'JavaScript', 'React', 'Node.js', 'CSS', 'HTML',
-        'Python', 'Java', 'Ruby', 'SQL', 'GraphQL', 'Docker',
-        'Kubernetes', 'AWS', 'Azure', 'Git', 'Webpack', 'Babel',
-        'Jest', 'MongoDB', 'Redis'
-    ],
-};
